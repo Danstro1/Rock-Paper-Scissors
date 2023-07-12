c4f67ecf94ae02b584dfa5dfdfc87b30aff523e8
Danstro1 <galactictimur@gmail.com>
2023-07-12 04:29:55 +0300

    Add new playRound function
    It takes player and computer selections as parameters
    and returns result of 1 Rock Paper Scissor game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let human = 0;
+let robot = 0;
+
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 3);
     switch(rand){
@@ -7,5 +10,45 @@ function getComputerChoice(){
             return 'Paper';
         case 2:
             return 'Scissors';
+    }
+}
+
+function playRound(playerSelection,computerSelection){
+    let player = playerSelection.trim()[0].toUpperCase() + playerSelection.trim().toLowerCase().slice(1);
+    if(player != "Paper" && player != 'Rock' && player != 'Scissors') {
+        return "You entered wrong"
+    }
+    if(player === computerSelection){
+        return "It's a Draw! \nPlayer: " + player + "\nComputer: " + computerSelection
+    }
+    switch(player){
+        case 'Rock':
+            if(computerSelection === 'Paper') {
+                robot++;
+                return "You Lose! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
+            if(computerSelection === 'Scissors') {
+                human++;
+                return "You Won! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
+        case 'Paper':
+            if(computerSelection === 'Scissors') {
+                robot++;
+                return "You Lose! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
+            if(computerSelection === 'Rock') {
+                human++;
+                return "You Won! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
+        case 'Scissors':
+            if(computerSelection === 'Rock') {
+                robot++;
+                return "You Lose! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
+
+            if(computerSelection === 'Paper') {
+                human++;
+                return "You Won! \nPlayer: " + player + "\nComputer: " + computerSelection;
+            }
     }
 }
